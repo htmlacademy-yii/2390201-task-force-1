68ba03dae5e34df497eede70e435497bd11183d4
romnosk <romnosk@gmail.com>
2025-09-05 08:55:57 +0300

    3.14 генерация скриптов SQL

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ require_once 'vendor/autoload.php';
 use Romnosk\Models\Task;
 use Romnosk\Models\Status;
 use Romnosk\Models\Action;
+use Romnosk\Models\CSVReader;
+use Romnosk\Models\DBTableGenerator;
 
 $customerId = 1;
 $executorId = 2;
@@ -28,6 +30,26 @@ foreach ($actions as $action => $actionObject) {
   $next = $task->getNextStatus($action);
   echo "-- {$actionObject->getLabel()} ({$action}) перейдёт в статус: " . $next->label() . "<br>";
 }
+
+$csvFilename = './data/categories.csv';
+$reader = new CSVReader($csvFilename);
+$row = $reader->next();
+while ($row !== false) {
+  echo json_encode($row, JSON_UNESCAPED_UNICODE)."<br>";
+  $row = $reader->next();
+}
+
+$tableName = 'specializations';
+$generator = new DBTableGenerator($csvFilename, $tableName);
+
+try {
+  $generator->generate();
+  echo "Данные успешно записаны в файл ".$generator->filename."<br>";
+} catch (RuntimeException $e) {
+  echo "Ошибка: " . $e->getMessage() . "<br>";
+}
+
+
 ?>
 </body>
 </html>
